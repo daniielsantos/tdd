5aef89a38f6057c702b4ae8b963befdd0b3fb28c
Daniel Souza <daniielsouzapvh@gmail.com>
2021-05-23 10:58:16 -0300

    test: husky test

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,7 +2,7 @@ export class SignUpcontroller {
   handle (httpResquest: any): any {
     return {
       statusCode: 400,
-      body: new Error('Missing param: name')
+      body: new Error('Missing param: nam')
     }
   }
 }
