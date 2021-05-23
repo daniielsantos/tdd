772b266fa05858a483893378020afd02a8c40fc6
Daniel Souza <daniielsouzapvh@gmail.com>
2021-05-23 12:22:14 -0300

    fix: husky

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -9,7 +9,7 @@ export class SignUpcontroller {
     if (!httpResquest.body.email) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: emaill')
+        body: new Error('Missing param: emai')
       }
     }
   }
