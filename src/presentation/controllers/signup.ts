f6b1438a916ea5abfc981a306ab3f9477ca0e0b1
Daniel Souza <daniielsouzapvh@gmail.com>
2021-05-23 14:07:51 -0300

    fix: huskyyyyy

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,16 +1,16 @@
 export class SignUpcontroller {
   handle (httpResquest: any): any {
-    if (!httpResquest.body.name) {
-      return {
-        statusCode: 400,
-        body: new Error('Missing param: name')
-      }
-    }
-    if (!httpResquest.body.email) {
-      return {
-        statusCode: 400,
-        body: new Error('Missing param: emaiaasd')
-      }
-    }
+    // if (!httpResquest.body.name) {
+    //   return {
+    //     statusCode: 400,
+    //     body: new Error('Missing param: name')
+    //   }
+    // }
+    // if (!httpResquest.body.email) {
+    //   return {
+    //     statusCode: 400,
+    //     body: new Error('Missing param: emaiaasd')
+    //   }
+    // }
   }
 }
